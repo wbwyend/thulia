937ffa0bbf09bbf5429d9b78de8e0740c1db4604
WBWY <1317378498@qq.com>
2023-09-16 18:28:05 +0800

    0.1

---
+++ server/src/main/java/com/wyer/server/controller/webController.java
@@ -0,0 +1,21 @@
+package com.wyer.server.controller;
+
+import com.wyer.server.common.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Function:
+ * Writer: wyer
+ * Date: 2023/09/16 16:58
+ **/
+@RestController
+public class webController {
+    @RequestMapping
+    public Result get() {
+        return Result.success();
+    }
+
+}
