9490465f6316002bae412301993affd044e918bd
WBWY <1317378498@qq.com>
2024-05-10 17:23:07 +0800

    2.0

--- server/src/main/java/com/wyer/server/config/COSConfig.java
+++ server/src/main/java/com/wyer/server/config/COSConfig.java
@@ -18,12 +18,34 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "cos")
 public class COSConfig {
 
+    public COSConfig() {
+    }
+
+    public String getSecretId() {
+        return secretId;
+    }
+
+    public void setSecretId(String secretId) {
+        this.secretId = secretId;
+    }
+
+    private String secretId;
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    private String secretKey;
+
+
     @Bean
     public COSClient getCOSClient() {
         // 1 初始化用户身份信息（secretId, secretKey）。
         // SECRETID 和 SECRETKEY 请登录访问管理控制台 https://console.cloud.tencent.com/cam/capi 进行查看和管理
-        String secretId = "AKID75rQrBNROI5mMpBTebqytjgjmlomxHlT";//用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
-        String secretKey = "wmQsO5e7Wr5E2f8f8xZ3UlwujaGi3DQZ";//用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
         COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
         // 2 设置 bucket 的地域, COS 地域的简称请参见 https://cloud.tencent.com/document/product/436/6224
         // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
